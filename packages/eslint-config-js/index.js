1874ed8942bb1d3b20ccd558d71fb38d3c885b54
Lovro Bikic <lovro.bikic@infinum.hr>
2021-12-14 19:20:28 +0100

    Update env and rules

--- packages/eslint-config-js/index.js
+++ packages/eslint-config-js/index.js
@@ -2,4 +2,17 @@ module.exports = {
   root: true,
   extends: ['eslint:recommended'],
   parser: '@babel/eslint-parser',
+  env: {
+    node: true,
+    commonjs: true,
+    jquery: true,
+    browser: true,
+    es6: true,
+    es2017: true,
+    es2020: true
+  },
+  rules: {
+    semi: 'error',
+    eqeqeq: ['error', 'smart']
+  }
 };
