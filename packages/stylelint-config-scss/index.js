8e22ce86d5ff54c6fbb42e2d3d0de42deddfa666
Lovro Bikic <lovro.bikic@infinum.hr>
2021-12-21 10:37:23 +0100

    Allow import of .min files

--- packages/stylelint-config-scss/index.js
+++ packages/stylelint-config-scss/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: 'stylelint-config-standard-scss',
   rules: {
-    "selector-list-comma-newline-after": "never-multi-line"
+    "selector-list-comma-newline-after": "never-multi-line",
+    "at-import-partial-extension-whitelist": ["min"]
   }
 };
