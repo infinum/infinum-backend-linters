19d75b7e42be881c4ecaa980b6a5ab885a304cdd
Lovro Bikic <lovro.bikic@infinum.hr>
2021-12-21 12:16:05 +0100

    Disable some dollar variables rules

--- packages/stylelint-config-scss/index.js
+++ packages/stylelint-config-scss/index.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: 'stylelint-config-standard-scss',
   rules: {
     "selector-list-comma-newline-after": "never-multi-line",
-    "at-import-partial-extension-whitelist": ["min"]
+    "at-import-partial-extension-whitelist": ["min"],
+    "scss/dollar-variable-empty-line-before": null,
+    "scss/dollar-variable-colon-space-after": null
   }
 };
