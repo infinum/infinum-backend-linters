759da4470cbe7d3673083bd92bb729a108cc549d
Lovro Bikic <lovro.bikic@infinum.hr>
2021-12-21 18:54:25 +0100

    Consistently use double quotes everywhere

--- packages/stylelint-config-scss/index.js
+++ packages/stylelint-config-scss/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'stylelint-config-standard-scss',
+  extends: "stylelint-config-standard-scss",
   rules: {
     "selector-list-comma-newline-after": "never-multi-line",
     "scss/at-import-partial-extension-whitelist": ["min"],
