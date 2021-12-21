d06598f8fcf220264c36cd06ffdf8d613358b533
Lovro Bikic <lovro.bikic@infinum.hr>
2021-12-21 12:19:03 +0100

    Prefix rule name with scss/

--- packages/stylelint-config-scss/index.js
+++ packages/stylelint-config-scss/index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: 'stylelint-config-standard-scss',
   rules: {
     "selector-list-comma-newline-after": "never-multi-line",
-    "at-import-partial-extension-whitelist": ["min"],
+    "scss/at-import-partial-extension-whitelist": ["min"],
     "scss/dollar-variable-empty-line-before": null,
     "scss/dollar-variable-colon-space-after": null
   }
